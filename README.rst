1ec9db6d546a4c889b20f9381f7f573f96e704a9
Damien Nicolas <damien@gordon.re>
2015-12-04 16:10:49 +0100

    Bootstrap project structure

---
+++ README.rst
@@ -0,0 +1,4 @@
+SublimePost
+===========
+
+I love cheese. Also, you should not read this yet.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(name='sublimepost',
+                 version='0.1.0',
+                 description='New generation content management system',
+                 long_description=open('README.rst').read().strip(),
+                 author='Sam, Max & friends',
+                 author_email='lesametlemax@gmail.com',
+                 url='https://github.com/sametmax/sublimepost/',
+                 py_modules=['sublimepost'],
+                 install_requires=['aiohttp', ],
+                 extras_require={
+                     'dev': ['sphinx', 'tox', 'pytest']
+                 },
+                 license='WTFPL',
+                 zip_safe=False,
+                 keywords='sublimepost blogging microblogging async',
+                 classifiers=['Development Status :: 1 - Planning',
+                              'Intended Audience :: Developers',
+                              'Natural Language :: English',
+                              'Programming Language :: Python :: 3.5',
+                              'Programming Language :: Python :: 3 :: Only',
+                              'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+                              'Topic :: Internet :: WWW/HTTP :: Site '
+                              'Management',
+                              'Operating System :: OS Independent'],)
