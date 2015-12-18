7718388af9c195034ef35809da45c5b980c0f769
Damien Nicolas <damien@gordon.re>
2015-12-18 18:31:11 +0100

    WIP: Refactor using Tygs

--- src/sublimepost/__main__.py
+++ src/sublimepost/__main__.py
@@ -1,25 +1,11 @@
-from aiohttp import web
-import jinja2
-import aiohttp_jinja2
-from path import Path
-
-from sublimepost.server import Server
+from tygs import WebApp
 
 
-@aiohttp_jinja2.template('hello.jinja2')
-async def hello(request):
-    return {'name': 'world'}
-
-app = web.Application()
-aiohttp_jinja2.setup(app,
-                     loader=jinja2.FileSystemLoader(Path(__file__).parent
-                                                    / 'templates'))
-app.router.add_route('GET', '/', hello)
+app, http = WebApp.quickstart('sublimepost')
 
 
-def main():
-    srv = Server(app)
-    srv.run()
+@http.get('/')
+async def hello(req, res):
+    res.render('hello', {'name': 'world'})
 
-if __name__ == '__main__':
-    main()
+app.ready()
