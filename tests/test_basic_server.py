05bceb5cf339e6c8c2b73291ac591f809da69d29
Damien Nicolas <damien@gordon.re>
2016-01-02 16:19:38 +0100

    Update tests with Tygs

--- tests/test_basic_server.py
+++ tests/test_basic_server.py
@@ -12,34 +12,22 @@ from sublimepost.server import Server
 
 @pytest.yield_fixture(scope='module', autouse=True)
 def start_server():
-    srv = Server(app)
-    t = Process(target=srv.run)
+    print('Before process')
+    t = Process(target=app.ready)
+    print('After process creation')
     t.start()
-    sleep(1)
+    print('After process start')
+    sleep(3)
+    print('After slip')
     yield
+    print('After yield')
     # srv.stop()
     t.terminate()
-    # t.join()
+    print('After terminate')
+    t.join()
+    print('After join')
 
 
 def test_run():
     req = requests.get('http://localhost:8080')
     assert b'Hello world' in req.content
-
-def test_server():
-    srv = Server(app)
-    srv.loop = MagicMock()
-    srv.server = MagicMock()
-    srv.app = MagicMock()
-    srv.handler = MagicMock()
-    c = srv.loop.run_until_complete.call_count
-    srv.get_server()
-    assert srv.loop.run_until_complete.call_count == c + 1, 'run_until_complete has not been called since server fetch'
-    c2 = srv.loop.run_forever.call_count
-
-    srv._run_server()
-    assert srv.loop.run_forever.call_count == c2 + 1, 'run_forever has not been called since run call'
-    assert srv.loop.close.call_count == 1
-    assert srv.server.close.call_count == 1
-    assert srv.handler.finish_connections.call_count == 1
-    assert srv.app.finish.call_count == 1
